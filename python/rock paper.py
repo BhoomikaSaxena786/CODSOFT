9b5fb749d7f8c678e2e2423388010c1521168e99
Bhoomika Saxena <147382614+BhoomikaSaxena786@users.noreply.github.com>
2023-10-23 15:08:01 +0530

    Add files via upload

---
+++ python/rock paper.py
@@ -0,0 +1,36 @@
+# ROCK PAPER SCISSORS GAME -->
+
+"""
+Computer choice = rock/paper/scissors
+User choice = input()
+
+if 
+elif
+..
+..
+else
+"""
+# IF_ELIF
+import random
+
+options = ["rock", "paper", "scissors"]
+
+computer_choice = random.choice(options)
+user_choice = input("Enter rock/paper/scissors = ")
+
+if user_choice != "paper" and user_choice != "rock" and user_choice != "scissors":
+    print("Invalid Choice")
+elif user_choice == "paper" and computer_choice == "rock":
+    print("You won")
+elif user_choice == "rock" and computer_choice == "scissors":
+    print("You won")
+elif user_choice == "scissors" and computer_choice == "paper":
+    print("You won")
+elif user_choice == computer_choice:
+    print("You both tied")
+else:
+    print(f"You lost, computer choice was {computer_choice}")
+
+
+
+
